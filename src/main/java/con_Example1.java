ca8cda6716b705492315219cc778b6ad249970be
santhoshrajkovuru2025 <srkovuru@gmail.com>
2025-09-19 14:29:26 +0530

    Added Constructor and Operators Programs

---
+++ src/main/java/con_Example1.java
@@ -0,0 +1,23 @@
+class Employee {
+
+    String Name;
+    int id;
+
+    Employee() {
+
+        System.out.println("Employee constructor");
+    }
+    Employee(String name, int id){
+        this.Name=name;
+        this.id=id;
+    }
+}
+
+public class con_Example1 {
+    public static void main(String[] args) {
+        Employee e =new Employee();
+        Employee e1 = new Employee("Santhosh",1984);
+        System.out.println(e.Name);
+        System.out.println(e.id);
+    }
+}
