e1e7a71fbc4f6f5ff342209362c6c751e7597a9d
santhoshrajkovuru2025 <srkovuru@gmail.com>
2025-09-19 12:31:09 +0530

    Enter a reason to commit

---
+++ src/main/java/Constructor.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+
+public class Constructor {
+
+    int a;
+    String str;
+
+    Constructor(int a, String str) {
+        this.a = a;
+        this.str = str;
+    }
+    public static void main(String[] args) {
+
+        Constructor c = new Constructor(1, "abc");
+        System.out.println(c.a);
+        System.out.println(c.str);
+
+
+
+        }
+    }
+
